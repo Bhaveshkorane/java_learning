426e965cbffaec87d34d964bc02d30539155facb
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-09-23 17:56:15 +0530

    worked on array in java

---
+++ array.java
@@ -0,0 +1,49 @@
+public class array {
+    public static void main(String args[])
+    {
+        int num[] = new int[4];
+        num[0] = 10;
+        num[1] = 20;
+        num[2] = 30;
+        num[3] = 40;
+
+        System.out.print("The array is :");
+        
+        for(int i=0;i<4;i++)
+        {
+            System.out.print(i+" ");
+        }
+
+        System.out.print("\nThe num1 arrays is:");
+
+        int num1[] = {1,2,3,4,5,6,7,8};
+        for(int i=0;i<8;i++)
+        {
+            System.out.print(num1[i]+" ");
+        }
+    }
+    
+}
+
+class multiDArray{
+    public static void main(String args[])
+    {
+        int num[][] = new int[2][2];
+        num[0][0] = 10;
+        num[0][1] = 20;
+        num[1][0] = 30;
+        num[1][1] = 40;
+
+        System.out.println("the multidimensional array is:");
+
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+
+            {
+                System.out.print(num[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
