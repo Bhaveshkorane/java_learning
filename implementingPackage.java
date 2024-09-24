46638fe30123eb43e43d118b2f7f0cb0a7404cc4
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-09-24 17:39:48 +0530

    implemented packages

---
+++ implementingPackage.java
@@ -0,0 +1,11 @@
+import tools.Calc;
+public class implementingPackage {
+
+    public static void main(String args[]){
+
+    Calc obj =  new Calc(10,20);
+    obj.add();
+    }
+
+    
+}
---
+++ tools/Calc.java
@@ -0,0 +1,16 @@
+package tools;
+
+public class Calc {
+    int a;
+    int b;
+    public Calc(int a,int b){
+        this.a = a;
+        this.b =b;
+    }
+
+    public void add()
+    {
+        System.out.println("the addition is :"+(this.a+this.b));
+    }
+    
+}
