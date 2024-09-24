3e80917506badd31650b419e16a3af7b0fdcee8e
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-09-24 15:54:30 +0530

    added instance mehtod,static mehtod and abstract methods

--- methodsInJava.java
+++ methodsInJava.java
@@ -1,3 +1,11 @@
+/*Methods implemented
+ * 1.Instance method
+ * 2.Static methods 
+ * 3.Abstract mehtods 
+ * 
+ */
+
+
 
 class Fun{
     int a,b;
@@ -6,17 +14,42 @@ class Fun{
         this.b = b;
 
     }
-    public int add()
+    public int add() //This is instance method 
     {
         return this.a+this.b;
     }
+
 }
 
-public class methodsInJava {
+abstract class multiply{
+    abstract void mul();
+
+}
+
+
+public class methodsInJava extends multiply{
+
+    void mul()
+    {
+        System.out.println("the multiplication method is being called which is abstract");
+    }
+
+    static void display(){//static method 
+        System.out.println("This is static mehtod and calling it in main ");
+    }
+    void hello(){//instance method
+        System.out.println("This is instance method and calling it in main ");
+    }
     public static void main(String[] args)
     {
         Fun f = new Fun(5,6);
-        System.out.println(f.add());
+        System.out.println("The addition of numbers is:"+f.add());
+        display();
+
+        //object creation to call  instance method and abstrac method 
+        methodsInJava obj = new methodsInJava();
+        obj.hello();
+        obj.mul();
         
     }
     
