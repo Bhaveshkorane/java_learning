a9249b0e43b41b646367de0546c9690e15f907f6
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-09-25 10:46:37 +0530

    implemented static method in java

---
+++ StaticMethod.java
@@ -0,0 +1,40 @@
+public class StaticMethod {
+
+    static class Student{
+
+        static int count;
+    
+        String name;
+        int age;
+        int roll;
+    
+    
+        void show(){
+            System.out.println("name: "+this.name+" age: "+this.age+" roll: "+this.roll);
+        }
+
+        public static void show1(Student obj){
+            System.out.println("name: "+obj.name+" age: "+obj.age+" roll: "+obj.roll);
+        }
+    }
+    public static void main(String args[]){
+        Student s1 = new Student();
+        s1.name  = "bhavesh";
+        s1.age = 20;
+        s1.roll = (Student.count+=1);
+    
+        Student s2 = new Student();
+        s2.name =  "Abhi";
+        s2.age = 21;
+        s2.roll = (Student.count+=1);
+    
+    
+        // s1.show();
+        // s2.show();
+
+        Student.show1(s1);
+        Student.show1(s2);
+    
+    }
+    
+}
